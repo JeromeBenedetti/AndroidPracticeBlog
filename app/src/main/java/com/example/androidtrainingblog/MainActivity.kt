8b8cc5167b00c3f6e536629f1a7beccbed51126b
Jérôme Benedetti <benedetti.jerome@laposte.net>
2025-07-01 00:51:29 +0200

    feat: add article component
     Conflicts:
    	app/src/main/java/com/example/androidtrainingblog/MainActivity.kt

--- app/src/main/java/com/example/androidtrainingblog/MainActivity.kt
+++ app/src/main/java/com/example/androidtrainingblog/MainActivity.kt
@@ -5,17 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidtrainingblog.ui.theme.AndroidTrainingBlogTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,8 +32,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             AndroidTrainingBlogTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
                     Column {
                         Head(Modifier.height(128.dp))
+                        Article(
+                            title = stringResource(R.string.title),
+                            introduction = stringResource(R.string.introduction),
+                            content = stringResource(R.string.content),
+                        )
                     }
                 }
             }
@@ -46,10 +59,45 @@ fun Head(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun Article(
+    title: String,
+    introduction: String,
+    content: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier,
+        verticalArrangement = Arrangement.Top,
+    ) {
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp,
+        )
+        Text(
+            text = introduction,
+            modifier = Modifier.padding(horizontal = 16.dp),
+            fontSize = TextUnit.Unspecified,
+            textAlign = TextAlign.Justify,
+        )
+        Text(
+            text = content,
+            modifier = Modifier.padding(16.dp),
+            fontSize = TextUnit.Unspecified,
+            textAlign = TextAlign.Justify,
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
-fun HeadPreview() {
+fun ArticlePreview() {
     AndroidTrainingBlogTheme {
-        Head()
+        Article(
+            title = stringResource(R.string.title),
+            introduction = stringResource(R.string.introduction),
+            content = stringResource(R.string.content),
+        )
     }
 }
